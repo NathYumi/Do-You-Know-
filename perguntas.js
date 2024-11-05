0e3698c26c92e89568e458418baf1d76590f8658
NathYumi <00001130850730sp@al.educacao.sp.gov.br>
2024-11-05 18:50:57 -0300

    Add files via upload
    you know :)

---
+++ perguntas.js
@@ -0,0 +1,35 @@
+criaCartao(
+    'Filosofia',
+    'De quem é a famosa frase “Penso, logo existo”?',
+    '“Je pense, donc je suis” é a frase original, escrita em francês, do filósofo René Descartes (1596-1650)'
+)
+
+criaCartao(
+    'Matemática',
+    'Quantas casas decimais tem o número pi?',
+    'Infinita uai ¯\_(ツ)_/¯'
+)
+
+criaCartao(
+    'Física',
+    'Qual a velocidade da luz?',
+    '299 792 458 metros por segundo (m/s). Só não é mais rápido que o cachorro da rua que vem correndo atrás de mim, infeliz 😔'
+)
+
+criaCartao(
+    'Biologia',
+    'Quais são os cromossomos que determinam o sexo masculino?',
+    'Os cromossomos sexuais são dois: o “X” é o cromossomo feminino e “Y”, o cromossomo masculino.'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual é o menor e o maior país do mundo?',
+    'Vaticano e Rússia. ✋✊✋✊'
+)
+
+criaCartao(
+    'História',
+    'Em que período da pré-história o fogo foi descoberto?',
+    'Paleolítico. (Primeiro dia espalhando fake news e dormindo tranquila)'
+)
